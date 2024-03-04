39f028f1a0af2cb5180b22f94d25e370ab9e5464
saifil <saifilmomin4@gmail.com>
2024-03-03 21:39:02 -0500

    added title component

---
+++ components/ui/Title.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Hero({ children }: { children: React.ReactNode }) {
+    return (
+        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+            {children}
+        </h1>
+    );
+}
+
