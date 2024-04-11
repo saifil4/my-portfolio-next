32d4ed6e9b2f649d7ea54c21616128eba78f7989
saifil <saifilmomin4@gmail.com>
2024-04-10 21:59:12 -0400

    fixes in ui and removed unused imaged

--- app/home/Hero.tsx
+++ app/home/Hero.tsx
@@ -12,9 +12,12 @@ export default function Hero() {
             />
             <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
                 <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-                    Hello, I am Saifil <br /> A Software Engineer <br/> Based in Toronto
+                    Hello, I am Saifil
                 </h1>
-                <br/>
+                <h2 className="font-extralight text-base md:text-4xl text-neutral-200 py-4 text-center">
+                    I am a Front end Developer <br />who constantly seeks to improve and learn new things.
+                </h2>
+                <br />
                 <div className='flex justify-center'>
                     <Button onClick={() => { }} text='Download Resume' />
                 </div>
--- app/home/Work.tsx
+++ app/home/Work.tsx
@@ -8,14 +8,16 @@ import Title from '@/components/ui/Title';
 
 export default function Work() {
     return (
-        <div className='bg-gray-900 p-12 grid grid-cols-2' >
+        <div className='bg-gray-900 p-12' >
             <Title >Projects</Title>
-            <br/>
-            {
-                ProjectsData.map((project, index) => (
-                    <Card key={index} project={project} />
-                ))
-            }
+            <br />
+            <div className="grid grid-cols-2">
+                {
+                    ProjectsData.map((project, index) => (
+                        <Card key={index} project={project} />
+                    ))
+                }
+            </div>
         </div>
     );
 }
