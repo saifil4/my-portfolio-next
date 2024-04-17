462688b0f00cc28b01b58534e65a810db6d143aa
saifil <saifilmomin4@gmail.com>
2024-04-16 21:39:33 -0400

    removed contact field

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import Hero from './home/Hero';
-import Contact from './home/Contact';
 import Work from './home/Work';
 import Skills from "./home/Skills";
 import About from "./home/About";
@@ -11,10 +10,9 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <About />
       <Work />
       <Skills />
-      <Contact />
+      <About />
     </>
   );
 }
